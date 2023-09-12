a09536e051a1e585fa63e5000e187885dbf66f6c
palashkumardas <info.palash27@gmail.com>
2023-09-12 10:58:26 +0600

    test complete jwt

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -65,7 +65,7 @@ class TaskController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified resource from storage...
      */
     public function destroy($id)
     {
